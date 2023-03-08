320334af0a75d2ac2d60bdf5737dbe6189c8cb4a
LeHaine <colt.daily@gmail.com>
2023-03-08 14:29:54 -0500

    convert GridPoint to value class
    update AStar to be more GC friendly

--- core/src/commonMain/kotlin/com/lehaine/littlekt/extras/GridPoint.kt
+++ core/src/commonMain/kotlin/com/lehaine/littlekt/extras/GridPoint.kt
@@ -1,7 +1,48 @@
 package com.lehaine.littlekt.extras
 
+import kotlin.jvm.JvmInline
+
 /**
  * @author Colton Daily
  * @date 3/11/2022
  */
-data class GridPoint(var cx: Int, var cy: Int, var xr: Float = 0.5f, var yr: Float = 0.5f)
+@JvmInline
+value class GridPoint internal constructor(@PublishedApi internal val packedValue: Long) : Comparable<GridPoint> {
+
+    val cx: Int get() = unpackInt(packedValue, 0)
+    val cy: Int get() = unpackInt(packedValue, 1)
+    val width: Int get() = unpackInt(packedValue, 2)
+    val coordId: Int get() = cx + cy * width
+
+    override fun compareTo(other: GridPoint): Int = coordId.compareTo(other.coordId)
+
+    override fun toString(): String {
+        return "GridPoint(cx=$cx, cy=$cy, width=$width)"
+    }
+
+    companion object {
+        operator fun invoke(cx: Int, cy: Int, gridWidth: Int) =
+            GridPoint(packInts(cx, cy, gridWidth))
+
+        private fun packInts(a: Int, b: Int, c: Int): Long {
+            return (a.toLong() shl 32) or (b.toLong() shl 16) or c.toLong()
+        }
+
+        private fun unpackInt(value: Long, idx: Int): Int {
+            return when (idx) {
+                0 -> {
+                    (value shr 32) and 0xFFFFL
+                }
+
+                1 -> {
+                    (value shr 16) and 0xFFFFL
+                }
+
+                else -> {
+                    value and 0xFFFFL
+                }
+            }.toInt()
+        }
+    }
+}
+
