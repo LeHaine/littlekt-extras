1b2a5b6dbd24fb44c4386d0f2a16e5a927f79467
LeHaine <colt.daily@gmail.com>
2023-03-10 15:36:52 -0500

    ecs: cleanup

--- core/src/commonMain/kotlin/com/lehaine/littlekt/extras/ecs/event/Events.kt
+++
@@ -1,8 +0,0 @@
-package com.lehaine.littlekt.extras.ecs.event
-
-sealed class LevelCollision(val x: Int, val y: Int) {
-    object Left : LevelCollision(-1, 0)
-    object Right : LevelCollision(1, 0)
-    object Top : LevelCollision(0, -1)
-    object Bottom : LevelCollision(0, 1)
-}
