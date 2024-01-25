d2b2df3bc431c33f88c6004c111a5ff722be3d1f
LeHaine <colt.daily@gmail.com>
2024-01-25 12:53:47 -0500

    shaders: tweak PixelSmoothShaders

--- core/src/commonMain/kotlin/com/lehaine/littlekt/extras/shader/PixelSmoothCameraTextureArrayShaders.kt
+++ core/src/commonMain/kotlin/com/lehaine/littlekt/extras/shader/PixelSmoothCameraTextureArrayShaders.kt
@@ -4,16 +4,17 @@ import com.lehaine.littlekt.graphics.shader.FragmentShaderModel
 import com.lehaine.littlekt.graphics.shader.ShaderParameter
 import com.lehaine.littlekt.graphics.shader.VertexShaderModel
 
-class PixelSmoothVertexShader : VertexShaderModel() {
+class PixelSmoothVertexTextureArrayShader : VertexShaderModel() {
     val uProjTrans = ShaderParameter.UniformMat4("u_projTrans")
     val uTextureSizes = ShaderParameter.UniformVec4("u_textureSizes")
     val uSampleProperties = ShaderParameter.UniformVec4("u_sampleProperties")
     val aPosition = ShaderParameter.Attribute("a_position")
     val aColor = ShaderParameter.Attribute("a_color")
     val aTexCoord0 = ShaderParameter.Attribute("a_texCoord0")
+    val aTextureIndex = ShaderParameter.Attribute("a_textureIndex")
 
     override val parameters: LinkedHashSet<ShaderParameter> =
-        linkedSetOf(uProjTrans, uTextureSizes, uSampleProperties, aPosition, aColor, aTexCoord0)
+        linkedSetOf(uProjTrans, uTextureSizes, uSampleProperties, aPosition, aColor, aTexCoord0, aTextureIndex)
 
     // language=GLSL
     override var source: String = """
@@ -23,8 +24,10 @@ class PixelSmoothVertexShader : VertexShaderModel() {
         attribute vec4 a_position;
         attribute vec4 a_color;
         attribute vec2 a_texCoord0;
+        attribute float a_textureIndex;
         varying lowp vec4 v_color;
         varying vec2 v_texCoords;
+        varying float v_textureIndex;
 
         void main()
         {
@@ -35,6 +38,7 @@ class PixelSmoothVertexShader : VertexShaderModel() {
 
             v_texCoords.x = a_texCoord0.x + u_sampleProperties.z / uvSize.x;
             v_texCoords.y = a_texCoord0.y - u_sampleProperties.w / uvSize.y;
+            v_textureIndex = a_textureIndex;
 
             gl_Position = u_projTrans * a_position;
         }
@@ -42,18 +46,19 @@ class PixelSmoothVertexShader : VertexShaderModel() {
 }
 
 
-class PixelSmoothFragmentShader : FragmentShaderModel() {
-    val uTexture = ShaderParameter.UniformSample2D("u_texture")
+class PixelSmoothFragmentTextureArrayShader : FragmentShaderModel() {
+    val uTextureArray = ShaderParameter.UniformSample2D("u_textureArray")
     val uTextureSizes = ShaderParameter.UniformVec4("u_textureSizes")
     val uSampleProperties = ShaderParameter.UniformVec4("u_sampleProperties")
 
-    override val parameters: LinkedHashSet<ShaderParameter> = linkedSetOf(uTexture, uTextureSizes, uSampleProperties)
+    override val parameters: LinkedHashSet<ShaderParameter> = linkedSetOf(uTextureArray, uTextureSizes, uSampleProperties)
 
     // language=GLSL
     override var source: String = """
-        uniform sampler2D u_texture;
+        uniform sampler2DArray u_textureArray;
         uniform vec4 u_textureSizes;
         uniform vec4 u_sampleProperties;
+        varying float v_textureIndex;
         varying lowp vec4 v_color;
         varying vec2 v_texCoords;
 
@@ -65,10 +70,10 @@ class PixelSmoothFragmentShader : FragmentShaderModel() {
             float dU = 1.0 / uvSize.x;
             float dV = 1.0 / uvSize.y;
 
-            vec4 c0 = texture2D(u_texture, uv);
-            vec4 c1 = texture2D(u_texture, uv + vec2(dU, 0));
-            vec4 c2 = texture2D(u_texture, uv + vec2(0, dV));
-            vec4 c3 = texture2D(u_texture, uv + vec2(dU, dV));
+            vec4 c0 = texture(u_textureArray, vec3(uv, v_textureIndex));
+            vec4 c1 = texture(u_textureArray, vec3(uv + vec2(dU, 0), v_textureIndex));
+            vec4 c2 = texture(u_textureArray, vec3(uv + vec2(0, dV), v_textureIndex));
+            vec4 c3 = texture(u_textureArray, vec3(uv + vec2(dU, dV), v_textureIndex));
 
             float subU = u_sampleProperties.x;
             float subV = u_sampleProperties.y;
@@ -80,7 +85,7 @@ class PixelSmoothFragmentShader : FragmentShaderModel() {
 
             vec4 bilinear = c0 * w0 * w2 + c1 * w1 * w2 + c2 * w0 * w3 + c3 * w1 * w3;
 
-            gl_FragColor = bilinear * v_color;
+            gl_FragColor = bilinear;
         }
     """.trimIndent()
 }
