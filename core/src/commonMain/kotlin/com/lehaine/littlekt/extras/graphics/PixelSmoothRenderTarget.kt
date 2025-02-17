73014a713ba6b561d31229d165ed888a6e954f10
LeHaine <colt.daily@gmail.com>
2025-02-17 10:03:10 -0500

    rework PixemSmoothRenderTarget to not handle the underlying texture

--- core/src/commonMain/kotlin/com/lehaine/littlekt/extras/graphics/PixelSmoothRenderTarget.kt
+++ core/src/commonMain/kotlin/com/lehaine/littlekt/extras/graphics/PixelSmoothRenderTarget.kt
@@ -1,11 +1,7 @@
 package com.lehaine.littlekt.extras.graphics
 
 import com.littlekt.Context
-import com.littlekt.Releasable
 import com.littlekt.graphics.Camera
-import com.littlekt.graphics.EmptyTexture
-import com.littlekt.graphics.webgpu.Device
-import com.littlekt.graphics.webgpu.TextureFormat
 import com.littlekt.math.MutableVec2f
 import com.littlekt.math.nextPowerOfTwo
 import kotlin.math.min
@@ -15,16 +11,13 @@ import kotlin.math.min
  * @date 3/1/2023
  */
 class PixelSmoothRenderTarget private constructor(
-    device: Device,
-    format: TextureFormat,
     val width: Int,
     val height: Int,
     val pxWidth: Int,
     val pxHeight: Int
-) : Releasable {
+) {
     var ppu: Float = 1f
     val ppuInv: Float get() = 1f / ppu
-    val target = EmptyTexture(device, format, width, height)
 
     fun getWorldCoords(
         x: Int,
@@ -37,19 +30,13 @@ class PixelSmoothRenderTarget private constructor(
         out.y = (pxHeight / 100f) * ((100f / context.graphics.height) * y)
         out.x *= ppuInv
         out.y *= ppuInv
-        out.x = out.x - target.width * ppuInv * 0.5f + camera.position.x
-        out.y = out.y - target.height * ppuInv * 0.5f + camera.position.y
+        out.x = out.x - width * ppuInv * 0.5f + camera.position.x
+        out.y = out.y - height * ppuInv * 0.5f + camera.position.y
         return out
-    }
-
-    override fun release() {
-        target.release()
     }
 
     companion object {
         operator fun invoke(
-            device: Device,
-            format: TextureFormat,
             screenWidth: Int,
             screenHeight: Int,
             targetHeight: Int,
@@ -68,8 +55,6 @@ class PixelSmoothRenderTarget private constructor(
             val pxHeight = h
 
             return PixelSmoothRenderTarget(
-                device,
-                format,
                 pxWidth.nextPowerOfTwo,
                 pxHeight.nextPowerOfTwo,
                 pxWidth,
